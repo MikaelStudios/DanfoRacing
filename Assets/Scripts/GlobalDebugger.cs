f66f0c37a122f0e5660c3eff08dfaf27a135dbe9
MikaelStudios <55417652+MikaelStudios@users.noreply.github.com>
2021-02-11 11:24:01 +0100

    trying again

---
+++ Assets/Scripts/GlobalDebugger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class GlobalDebugger : MonoBehaviour
+{
+    public static GlobalDebugger instance;
+    public Vector3 camOffset;
+    public float distance = 2f;
+    public float height = 1.5f;
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(this);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(this);
+    }
+}
